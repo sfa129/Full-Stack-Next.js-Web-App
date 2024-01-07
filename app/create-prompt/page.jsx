7b072fe775afe356ddad343c16822d1867e26ed9
SYED FAIZ AHMED <sfahmed129@gmail.com>
2024-01-07 14:22:00 +0500

    Morning - 07-01-2024

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -5,8 +5,12 @@ import { useSession } from 'next-auth/react';  //which user is loggedin
 import { useRouter } from 'next/navigation';
 
 import Form from '@components/Form';
+import { Router } from 'next/router';
 
 const CreatePrompt = () => {
+    const router = useRouter();
+    const { data: session } = useSession();
+
     const [submitting, setSubmitting] = useState(false);
     const [post, setPost] = useState({
         prompt: '',
@@ -14,7 +18,27 @@ const CreatePrompt = () => {
     });
 
     const createPrompt = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
+    
+        try {
+            const response = await fetch('/api/prompt/new', {
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag
+                })
+            })
 
+            if(response.ok) {
+                router.push('/');
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setSubmitting(false);
+        }
     }
 
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-
+import Feed from '@components/Feed'
 
 const Home = () => {
   return (
@@ -12,6 +12,8 @@ const Home = () => {
             Promptopia is an open-source AI prompting tool for modern world to discover, create 
             & share creative prompts
         </p>
+
+        <Feed/>
     </section>
   )
 }
