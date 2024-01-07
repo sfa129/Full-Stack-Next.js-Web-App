7f93a9ece018b15c8bc308d901c481408c14f4c8
SYED FAIZ AHMED <sfahmed129@gmail.com>
2024-01-08 00:18:44 +0500

    CRUD Completed

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -38,3 +38,15 @@ export const PATCH = async (request, { params }) => {
     }
 }
 
+//DELETE (delete)
+export const DELETE = async (request, { params }) => {
+    try {
+        await connectToDB();
+
+        await Prompt.findByIdAndDelete(params.id);
+
+        return new Response("Prompt deleted successfully", { status: 200 })
+    } catch (error) {
+        return new Response("Failed to delete prompt", { status: 500 })
+    }
+}
